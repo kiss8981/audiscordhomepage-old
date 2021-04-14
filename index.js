bfcaa25f5ee34fc5da7d64db49c4d66dfbaa1124
kiss8981 <kdh89811@gmail.com>
2021-04-15 00:56:17 +0900

    edit css

--- index.js
+++ index.js
@@ -19,15 +19,9 @@ app.get("/donate", (req, res) => {
   res.render("donate", {bot: settings.website })
 })
 
-<<<<<<< HEAD
 app.get("/privacy", (req, res) => {
   res.render("privacy", {bot: settings.website })
 })
 
 const listener = server.listen(8080, function() {
-    console.log("Your app is listening on port " + listener.address().port);
-=======
-const listener = server.listen(port, function() {
-    console.log("Your app is listening on port " + port);
->>>>>>> 33e00f0f19d27e5e686d9a56085fd305ed568852
-})
+    console.log("Your app is listening on port " + listener.address().port);})
