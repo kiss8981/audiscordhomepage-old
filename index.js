baa2ddd548342ca6739feeafbe6444bb38876f5f
kiss8981 <kdh89811@gmail.com>
2021-04-14 14:13:03 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const settings = require("./settings.json");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render('index', { bot: settings.website })
+})
+
+app.get("/commands", (req, res) => {
+  res.render("commands", {bot: settings.website, commands: settings.commands })
+})
+app.get("/donate", (req, res) => {
+  res.render("donate", {bot: settings.website })
+})
+
+const listener = server.listen(8000, function() {
+    console.log("Your app is listening on port " + listener.address().port);
+})
